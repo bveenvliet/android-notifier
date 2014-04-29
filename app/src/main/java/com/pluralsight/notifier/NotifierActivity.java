b95a000eee6d1f5202b789d45f60f6a12d5aa322
Larry Schiefer <larry.schiefer@hiqes.com>
2014-04-28 22:09:17 -0500

    Initial implementation, provides simple icon plus title and content text on the status bar.  Must be manually cleared by the user.

--- app/src/main/java/com/pluralsight/notifier/NotifierActivity.java
+++ app/src/main/java/com/pluralsight/notifier/NotifierActivity.java
@@ -1,37 +1,155 @@
+/****************************************************************************
+ Copyright 2014 Pluralsight LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+****************************************************************************/
 package com.pluralsight.notifier;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-public class NotifierActivity extends Activity {
+public class NotifierActivity extends Activity implements View.OnFocusChangeListener, TextWatcher {
+    private int                 mCurID = -1;
+    private NotificationManager mMgr;
+    private EditText            mTitle;
+    private EditText            mContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //  Get the NotificationManager from our context so we can send and
+        //  clear our notifications as directed by the user.
+        mMgr = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //  Setup our UI components and hook up our listeners to ensure
+        //  we actually have content to send.
         setContentView(R.layout.activity_notifier);
+        mTitle = (EditText)findViewById(R.id.notify_title);
+        mTitle.setOnFocusChangeListener(this);
+        mTitle.addTextChangedListener(this);
+        mContent = (EditText)findViewById(R.id.notify_content);
+        mContent.setOnFocusChangeListener(this);
+        mContent.addTextChangedListener(this);
     }
 
+    @Override
+    protected void onPause() {
+        mMgr.cancelAll();
+        mCurID = -1;
+        super.onPause();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.notifier, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        boolean                 ret = false;
+        Notification            notify;
+
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        if (id == R.id.set_notify) {
+            Notification.Builder bldr = new Notification.Builder(getApplicationContext());
+            bldr.setContentTitle(mTitle.getText().toString());
+            bldr.setContentText(mContent.getText().toString());
+            bldr.setSmallIcon(android.R.drawable.ic_dialog_alert);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                //noinspection deprecation
+                notify = bldr.getNotification();
+            } else {
+                notify = bldr.build();
+            }
+            notify.flags |= Notification.FLAG_NO_CLEAR;
+            mCurID = (int)System.currentTimeMillis();
+
+            //  Now that we have created our notification with the requested details, send it
+            //  over to the NotificationManager for display.
+            mMgr.notify(mCurID, notify);
+            ret = true;
+        } else if (id == R.id.clear_notify) {
+            mMgr.cancel(mCurID);
+            mCurID = -1;
+            ret = true;
         }
-        return super.onOptionsItemSelected(item);
+
+        if (!ret) {
+            ret = super.onOptionsItemSelected(item);
+        } else {
+            //  Invalidate the options menu so the buttons are updated appropriately
+            invalidateOptionsMenu();
+        }
+
+        return ret;
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem setItem = menu.findItem(R.id.set_notify);
+        MenuItem clearItem = menu.findItem(R.id.clear_notify);
+
+        clearItem.setEnabled(isClearAvailable());
+        setItem.setEnabled(isSetAvailable());
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    private boolean isSetAvailable() {
+        String title = mTitle.getText().toString();
+        String content = mContent.getText().toString();
+        return !(title.isEmpty() || content.isEmpty()) && (mCurID == -1);
+    }
+
+    private boolean isClearAvailable() {
+        return (mCurID != -1);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus) {
+            invalidateOptionsMenu();
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        //  Ignore
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        //  Ignore
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        //  After any change, invalidate the options menu so the buttons are updated
+        invalidateOptionsMenu();
+    }
 }
